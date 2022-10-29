fd1c3aa46880f8f68e298605ec0a7d54810bf8f4
Sebastian Trujillo <ippo_sebas@hotmail.com>
2022-10-29 15:30:19 -0500

    Add icons in consultas view

--- src/views/_indexes/Atenciones/AtencionesIndex.vue
+++ src/views/_indexes/Atenciones/AtencionesIndex.vue
@@ -1,13 +1,18 @@
+<!--
+    Diagnósticos','mdi-medical-bag
+    'Pruebas','mdi-needle'
+    'Resultados','mdi-clipboard-pulse-outline' -->
+    
 <template>
     <v-container id="AtencionesIndex" class="fill-height" fluid>
-        <v-row align="center" justify="center">
+        <v-row justify="center">
             <v-card class="px-5 pb-5 pt-6">
                 <v-row class="align-items-center px-5">
                     <h3>Consultas</h3>
                 </v-row>
                 <v-row class="align-items-center px-5">
-                    <v-col >
-                        <router-link :to="{name: 'Registro Consulta'}" >
+                    <v-col>
+                        <router-link :to="{ name: 'Registro Consulta' }">
                             <v-btn color="primary">
                                 <v-icon left>mdi-account-plus</v-icon>
                                 Registrar Consulta
@@ -16,41 +21,69 @@
                     </v-col>
                     <v-spacer></v-spacer>
                     <v-col>
-                        <v-text-field class="py-0" v-model="search" append-icon="mdi-magnify"
-                        label="Search" single-line hide-details ></v-text-field>
+                        <v-text-field class="py-0" v-model="search" append-icon="mdi-magnify" label="Search" single-line
+                            hide-details></v-text-field>
                     </v-col>
                 </v-row>
-                <v-row>
-                    <v-col>
-                        <v-data-table :headers="pacienteHeaders.base" :items="collection.items" :expanded.sync="expanded" :search="search" @click:row="clickRow">
-                            <template v-slot:expanded-item="{ headers, item }">
-                              <td :colspan="headers.length" class="py-2" >
-                                <v-row class="align-items-center px-5">
-                                    <v-col v-for="header in pacienteHeaders.atencionDetail" v-bind:key="header.value"> 
-                                        <b>{{header.text}}:</b> 
-                                        <div v-for="subheader in header.detail" v-bind:key="subheader.value">
-                                            {{subheader.text}}: {{item[header.value][subheader.value]}}
-                                        </div>
-                                    </v-col>
-                                </v-row>
-                                <!-- {{item}} -->
-                              </td>
-                            </template>
-                            <template #[`item.gruposangrh`]="{ item }">{{ item.gruposang }} {{ item.rh }}</template>
-                            <!--https://stackoverflow.com/questions/61344980/v-slot-directive-doesnt-support-any-modifier-->
-                            <template #[`item.actions`]="{ item }">
-                                <router-link :to="'/consultas/update/'+item.id">
-                                    <v-btn color="primary" outlined x-small fab> 
-                                        <v-icon>mdi-pencil</v-icon>
-                                    </v-btn>
-                                </router-link>
-                                <v-btn  color="error" outlined x-small fab>
-                                        <v-icon class=" text-danger">mdi-delete</v-icon>
-                                    </v-btn>
-                            </template>
-                        </v-data-table>
-                    </v-col>
-                </v-row>
+                <br>
+                <!--comienzo del data table-->
+
+                <v-data-table :headers="pacienteHeaders.base" :items="collection.items" :expanded.sync="expanded"
+                    :search="search" @click:row="clickRow">
+                    <template v-slot:expanded-item="{ headers, item }">
+                        <td :colspan="headers.length" class="py-2">
+                            <v-row class="align-items-center px-5">
+                                <v-col  v-for="header in pacienteHeaders.atencionDetail" v-bind:key="header.value">
+                                    <b>{{ header.text }}:</b>
+                                    <div v-for="subheader in header.detail" v-bind:key="subheader.value">
+                                        {{ subheader.text }}: {{ item[header.value][subheader.value] }}
+                                    </div>
+                                </v-col>
+                            </v-row>
+                            <!-- {{item}} -->
+                        </td>
+                    </template>
+
+                    <template #[`item.gruposangrh`]="{ item }">{{ item.gruposang }} {{ item.rh }}</template>
+                    <!--https://stackoverflow.com/questions/61344980/v-slot-directive-doesnt-support-any-modifier-->
+
+                    <template #[`item.actions`]="{ item }">
+                        <v-row justify="center">
+                            <router-link :to="'/consultas/update/' + item.id">
+                                <v-btn color="primary" outlined x-small fab>
+                                    <v-icon>mdi-pencil</v-icon>
+                                </v-btn>
+                            </router-link>
+                            <v-btn color="error" outlined x-small fab>
+                                <v-icon class=" text-danger">mdi-delete</v-icon>
+                            </v-btn>
+                        </v-row>
+                    </template>
+
+                    <template #[`item.diagnosticos`]>
+                        <v-row justify="center">
+                            <v-btn color="green" @click="dialog=true" x-small fab>
+                                <v-icon>mdi-medical-bag</v-icon>
+                            </v-btn>
+                        </v-row>
+                    </template>
+
+                    <template #[`item.pruebas`]>
+                        <v-row justify="center">
+                            <v-btn color="green" outlined x-small fab>
+                                <v-icon>mdi-needle</v-icon>
+                            </v-btn>
+                        </v-row>
+                    </template>
+
+                    <template #[`item.resultados`]>
+                        <v-row justify="center">
+                            <v-btn color="green" outlined x-small fab>
+                                <v-icon>mdi-clipboard-pulse-outline</v-icon>
+                            </v-btn>
+                        </v-row>
+                    </template>
+                </v-data-table>
             </v-card>
         </v-row>
     </v-container>
@@ -59,7 +92,10 @@
 </script>
 <style scoped>
 .v-input.expanding-search {
-  transition: max-width 0.5s; }
-  .v-input.expanding-search.closed {
-    max-width: 70px; }
+    transition: max-width 0.5s;
+}
+
+.v-input.expanding-search.closed {
+    max-width: 70px;
+}
 </style>
